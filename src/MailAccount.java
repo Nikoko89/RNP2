a22dc1930d598164bcf8478b4bb67f63eb2d382e
Niko Khaled <niko_khaled@icloud.com>
2016-11-15 09:38:59 +0100

    Fetching Mails

--- src/MailAccount.java
+++ src/MailAccount.java
@@ -6,26 +6,29 @@ public class MailAccount {
     private final String host;
     private final int port;
 
-    public MailAccount(String user, String password, String host, int port){
+    public MailAccount(String user, String password, String host, int port) {
         this.user = user;
         this.password = password;
         this.host = host;
         this.port = port;
     }
 
-    public String getUser(){
+    public String getUser() {
+
         return user;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
 
-    public String getHost(){
+    public String getHost() {
+
         return host;
     }
 
-    public int getPaort(){
+    public int getPort() {
+
         return port;
     }
 }
--- src/MailBox.java
+++ src/MailBox.java
@@ -1,9 +1,18 @@
+import javax.mail.Folder;
+import javax.mail.Session;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.io.*;
+import java.util.*;
+import javax.activation.*;
+import javax.mail.*;
+import javax.mail.internet.*;
 
 public class MailBox {
     private List<MailAccount> accounts;
+    private Message[] messages;
+    private HashMap<MailAccount, Message[]> accountMails = new HashMap<>();
 
     public MailBox() {
         accounts = new ArrayList<>();
@@ -14,6 +23,41 @@ public class MailBox {
             String serverAdress = data.getProperty("server");
             int port = Integer.parseInt(data.getProperty("port"));
             MailAccount account = new MailAccount(user, pass, serverAdress, port);
+            accounts.add(account);
         }
+    }
+
+    public void fetchMails(MailAccount acc) {
+        Properties properties = new Properties();
+        properties.put("mail.pop3.host", acc.getHost());
+        properties.put("mail.pop3.port", acc.getPort());
+        properties.put("mail.pop3.starttls.enable", "true");
+        Session session = Session.getDefaultInstance(properties);
+
+        Store store = null;
+        try {
+            store = session.getStore( "pop3" );
+        } catch (NoSuchProviderException e) {
+            System.err.println("Could not find provider");
+        }
+        try {
+            store.connect(acc.getHost(),acc.getPort(), acc.getUser(), acc.getPassword());
+        } catch (MessagingException e) {
+            System.err.println("Could not connect to store");
+        }
+
+        Folder folder = null;
+        try {
+            folder = store.getFolder( "INBOX" );
+            folder.open( Folder.READ_ONLY );
+            messages = folder.getMessages();
+        } catch (MessagingException e) {
+            System.err.println("Could not open folder");
+        }
+
+        accountMails.put(acc, messages);
+
+
+
     }
 }
