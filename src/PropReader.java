f043243a5d36c5ccfc36cf0f825c9f55912375d6
Niko Khaled <niko_khaled@icloud.com>
2016-11-15 01:04:45 +0100

    SMTP and PropReader done

---
+++ src/PropReader.java
@@ -0,0 +1,37 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropReader {
+
+    InputStream inputStream;
+    Properties prop;
+
+    public PropReader(String fileName) {
+        try {
+            prop = new Properties();
+
+            inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
+
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + fileName + "' not found in the classpath");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            System.err.println("Inputstream could not be closed");
+        }
+
+    }
+
+    public Properties getProp() {
+        return prop;
+    }
+}
